b0f4fdfeb82bf5b59f8bc82702f06bf8855bcb1c
Gabriel Aires Guedes <airesgabriel@gmail.com>
2021-11-26 02:38:39 -0300

    fix Hardware::Pid::Status, get main system metrics

--- src/tasks/monitoring.cr
+++ src/tasks/monitoring.cr
@@ -1,22 +1,60 @@
-Schedule.job :monitoring, :every, 1.second do
-  memory = Hardware::Memory.new
-  p memory.used
-  p memory.percent.to_i
+Schedule.job :monitoring, :every, 5.second do
+  mem = Hardware::Memory.new
+  puts "* Memory"
+  puts ""
+  puts "** total   (MB) : #{mem.total / 1024}"
+  puts "** used    (MB) : #{mem.used / 1024}"
+  puts "** free    (MB) : #{mem.available / 1024}"
+  puts ""
 
   cpu = Hardware::CPU.new
-  pid_stat = Hardware::PID.new.stat
-  app_stat = Hardware::PID.new("firefox").stat
+  puts "* CPU"
+  puts ""
+  puts "** usage    (%) : #{cpu.usage!.to_i}"
+  puts ""
 
-  p cpu.usage!.to_i          # => 17
-  p pid_stat.cpu_usage!      # => 1.5
-  p app_stat.cpu_usage!.to_i # => 4
+  puts "* Processes"
+  puts ""
+  Hardware::PID.each do |pid|
+    next unless pid.name.size > 0
+    puts "** pid (number) : #{pid.number}"
+    puts "** pid   (name) : #{pid.name}"
+    puts "** pid    (cmd) : #{pid.command}"
+    puts "** pid  (cpu %) : #{pid.stat.cpu_usage!}"
+    puts "** pid    (mem) : #{pid.status.vmrss}"
+    puts "** pid(threads) : #{pid.status.threads}"
+    puts "** pid  (state) : #{pid.status.state}"
+    puts "** pid (parent) : #{pid.status.ppid}"
+    puts ""
+  end
 
-  pp Psutil.cpu_times
-  pp Psutil.virtual_memory
-  pp Psutil.disk_partitions
-  pp Psutil.disk_usage
-  pp Psutil.disk_io_counters
-  pp Psutil.host_info
-  pp Psutil.load_avg
-  pp Psutil.net_io_counters
+  puts "* Disk Usage"
+  puts ""
+  Psutil.disk_partitions.each do |partition|
+    du = Psutil.disk_usage partition.mountpoint
+    puts "** mountpoint   : #{du.path}"
+    puts "** fs_type      : #{partition.fstype}"
+    puts "** device       : #{partition.device}"
+    puts "** size    (Mb) : #{du.total / 1024 ** 2}"
+    puts "** used    (Mb) : #{du.used / 1024 ** 2}"
+    puts "** free    (Mb) : #{du.free / 1024 ** 2}"
+    puts "** usage    (%) : #{du.used_percent}"
+    puts ""
+  end
+
+  l_avg = Psutil.load_avg
+  puts "* Load Averages"
+  puts ""
+  puts "** 1 minute     : #{l_avg.load1}"
+  puts "** 5 minutes    : #{l_avg.load5}"
+  puts "** 15 minutes   : #{l_avg.load15}"
+  puts ""
+
+  net = Psutil.net_io_counters.select { |counter| counter.name == "all" }.first
+  puts "* Net I/O"
+  puts ""
+  puts "** received (Mb): #{net.bytes_recv / 1024 ** 2}"
+  puts "** sent     (Mb): #{net.bytes_sent / 1024 ** 2}"
+  puts "** packets  (In): #{net.packets_recv}"
+  puts "** packets (Out): #{net.packets_sent}"
 end
