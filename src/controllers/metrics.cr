5d0fbab86dc0713cbae86e16fbbe5dd0de0c0916
Gabriel Aires Guedes <airesgabriel@gmail.com>
2022-03-17 23:07:43 -0300

    updated metrics endpoints

--- src/controllers/metrics.cr
+++ src/controllers/metrics.cr
@@ -1,9 +1,50 @@
 class Metrics < Application
-  {% for model in [Host, Boot, Load, Memory, Net, Disk, Partition, Pid] %}
 
-    get {{ ("\"/" + model.stringify.downcase + "\"").id }}, {{ (":" + model.stringify.downcase).id }} do
-      render json: {{model}}.all
+  def index
+    @links = {
+      "Uptime"  => ["yellow", "assets/watch.svg"  ],
+      "Load"    => ["red", "assets/cpu.svg"       ],
+      "Memory"  => ["green", "assets/server.svg"  ],
+      "Network" => ["blue", "assets/wifi.svg"     ],
+      "Disk"    => ["gray", "assets/database.svg" ]
+    }
+
+    respond_with { html template("metrics.slang") }
+  end
+
+  def show
+
+    timeseries = case params["id"]
+    when "uptime"
+      b = Boot.all
+      t = b.map &.seconds
+      { uptime: t , time: t }
+    when "load"
+      l = Load.all
+      t = l.map &.seconds
+      { load_1m: l.map(&.load1), load_5m: l.map(&.load5), load_15m: l.map(&.load15), time: t }
+    when "memory"
+      m = Memory.all
+      t = m.map &.seconds
+      { total_memory: m.map(&.total_mb), used_memory: m.map(&.used_mb), free_memory: m.map(&.free_mb), time: t }
+    when "network"
+      n = Net.all
+      t = n.map &.seconds
+      { download: n.map(&.received_mb), upload: n.map(&.sent_mb), time: t }
+    when "disk"
+      usage = Hash(String, Array(Int64) | Array(Float64)).new
+      Partition.all.each do |p|
+        d = p.disk.all
+        usage["#{p.mountpoint}_time" ] = d.map &.seconds
+        usage["#{p.mountpoint}_total"] = d.map &.size_mb
+        usage["#{p.mountpoint}_used" ] = d.map &.used_mb
+        usage["#{p.mountpoint}_free" ] = d.map &.free_mb
+        usage["#{p.mountpoint}_usage"] = d.map &.usage
+      end
+      usage
     end
 
-  {% end %}
+    render json: timeseries
+  end
+
 end
